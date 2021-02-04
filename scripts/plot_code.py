c039b13e900c182938f29397b6b478fdac11c13f
Aidan Mattrick <aidan.mattrick@gmail.com>
2021-02-04 15:44:19 -0800

    Finished overall graph

--- scripts/plot_code.py
+++ scripts/plot_code.py
@@ -10,67 +10,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 
-#%%
-def build_overall_graph(summary_df, country_list, feat_list, year_list):
-    """Builds a bar chart summarizing certain countries, feature names (columns in the df)
-    and a time frame
-
-    Parameters
-    ----------
-    summary_df : Pandas.DataFrame
-        Dataframe with columns
-    country_list : list
-        List of country names to filter `summary_df` on
-    feat_list : list
-        List of features (column names in `summary_df`). If `None` use all 7 contributing features to happiness score
-    year_list : list
-        List of years to filter on
-
-    Returns
-    -------
-    fig : plotly.express.Figure
-    """
-    # ALl features to consider
-    all_feats = [
-        "gdp_per_capita",
-        "family",
-        "health_life_expectancy",
-        "freedom",
-        "perceptions_of_corruption",
-        "generosity",
-        "dystopia_residual",
-    ]
-
-    if feat_list is None:
-        feat_list = all_feats
-
-    # Filter to specified data, calculate means
-    filtered_df = (
-        filter_df(summary_df, country_list, feat_list, year_list)
-        .groupby("country")
-        .mean()
-        .reset_index()
-        .sort_values("happiness_score", ascending=True)
-    )
-    cols = list(set(all_feats).intersection(filtered_df.columns))
-    title_string = (
-        f"Average Happiness Score by Contributing Factors: {min(year_list)} to {max(year_list)}"
-        if len(year_list) > 1
-        else f"Happiness Score by Contributing Factor: {year_list[0]}"
-    )
-
-    fig = px.bar(
-        filtered_df,
-        x=cols,
-        y="country",
-        title=title_string,
-        orientation="h",
-    )
-
-    ## If wanting to move legend around, update layout
-    # fig.update_layout({"legend_orientation": "h", "margin": {"t": 40, "l": 50}})
-
-    return fig
 
 
 #%% For use in VS Code Interactive window
